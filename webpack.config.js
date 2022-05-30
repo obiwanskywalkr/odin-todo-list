6235de5793a748f3a9f59cfd9a4efcbcd061c116
obiwanskywalkr <gp47spd13@gmail.com>
2022-05-30 14:39:23 -0500

    Initial commit for config files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            logging: 'error',
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do List',
+            template: './src/index.html',
+            // favicon: './src/images/favicon.png'
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ttf)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+};
