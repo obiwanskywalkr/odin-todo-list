bca7ac77c7bbe1eb290c9c5270b753476b639880
obiwanskywalkr <gp47spd13@gmail.com>
2022-07-20 17:05:03 -0500

    Fix bugs and add mark as complete functionality

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,20 +1,34 @@
 const userLists = [
-    {title: 'List 1', 
+    {title: 'Test list', 
     description: 'foobar', 
-    tasks: [{title: 'Critical task', priority: 'critical', dueDate: '2022-07-19T16:20', list: 'List 1', subtasks: [{title: 'foo', subtask: 1}], notes: 'bar', completed: 'false', taskID: 1}], 
-    listID: 1},
-
-    {title: 'List 2', 
-    description: 'foobar!', 
-    tasks: [{title: 'Important task', priority: 'important', dueDate: '2022-10-22T16:20', list: 'List 2', subtasks: [{title: 'foo!', subtask: 1}], notes: 'bar!', completed: 'false', taskID: 2}], 
-    listID: 2},
-    
-    {title: 'List 3', 
-    description: 'foobar?', 
-    tasks: [{title: 'Regular task', priority: 'low', dueDate: '2022-10-22T16:20', list: 'List 3', subtasks: [{title: 'foo?', subtask: 1}], notes: 'bar?', completed: 'false', taskID: 3}], 
-    listID: 3},
+    tasks: [{title: 'Task', priority: 'critical', dueDate: '2022-10-22T16:20', list: 'Test list', subtasks: [{title: 'foo', completed: true, subtask: 1}], notes: 'bar', completed: false, taskID: 1}], 
+    listID: 1}
 ]
 
 const allTasks = [];
 
-export { userLists, allTasks }
+function updateLocalStorage() {
+    localStorage.clear();
+
+    const userList_string = JSON.stringify(userLists);
+
+    localStorage.setItem("userLists", userList_string);
+
+    console.log(localStorage);
+}
+
+
+const userList_parsed = JSON.parse(localStorage.getItem("userLists"));
+
+export { userLists, allTasks, updateLocalStorage }
+
+
+// {title: 'List 2', 
+// description: 'foobar!', 
+// tasks: [{title: 'Important task', priority: 'important', dueDate: '2022-10-22T16:20', list: 'List 2', subtasks: [{title: 'foo!', completed: false, subtask: 1}], notes: 'bar!', completed: false, taskID: 2}], 
+// listID: 2},
+
+// {title: 'List 3', 
+// description: 'foobar?', 
+// tasks: [{title: 'Regular task', priority: 'low', dueDate: '2022-10-22T16:20', list: 'List 3', subtasks: [{title: 'foo?', completed: false, subtask: 1}], notes: 'bar?', completed: false, taskID: 3}], 
+// listID: 3},
